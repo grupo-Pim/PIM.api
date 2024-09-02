389620b818bb0eed1197f61802ac4348127974fd
Murilo Ribeiro <muriloribeiro11@gmail.com>
2024-09-01 21:04:05 -0300

    Ajuste da tabela usuarios com as demais

---
+++ Entidades/ColaboradorEntidade.cs
@@ -0,0 +1,24 @@
+﻿namespace PIM.api.Entidades
+{
+    public class ColaboradorEntidade
+    {
+        public ColaboradorEntidade()
+        {
+            
+        }
+        public ColaboradorEntidade(int empresaID, string nome, string login, 
+                                string senha, int funcao, bool serColaborador)
+        {
+            this.Funcao = funcao;
+            this.Usuario.SerColaborador = serColaborador;
+            this.Usuario.Nome = nome;
+            this.Usuario.Login = login;
+            this.Usuario.Senha = senha;
+            this.Usuario.EmpresaID = empresaID;
+        }
+        public int ID { get; set; }
+        public int Funcao { get; set; }
+        public UsuarioEntidade Usuario { get; set; }
+        public int UsuarioID { get; set; }
+    }
+}
