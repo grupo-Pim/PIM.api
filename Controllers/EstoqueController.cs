af4ed1ef00683411330362510730fe29295c66fb
Murilo Ribeiro <muriloribeiro11@gmail.com>
2024-11-10 17:10:52 -0300

    Estoque view

--- Controllers/EstoqueController.cs
+++ Controllers/EstoqueController.cs
@@ -1,6 +1,65 @@
-﻿namespace PIM.api.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PIM.api.Entidades;
+using PIM.api.Persistence;
+
+namespace PIM.api.Controllers
 {
-    public class EstoqueController
+    [Route("api/Estoque")]
+    [ApiController]
+    public class EstoqueController : ControllerBase
     {
+        private readonly FazendoDbContext _context;
+        public EstoqueController(FazendoDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Estoques(int empresaID)
+        {
+            bool empresaExiste = _context.Empresa.Any(o=> o.ID == empresaID);
+            if (empresaExiste) return BadRequest("Empresa não encontrado");
+
+            var listaEstoque = _context.Estoque.Include(o => o.Produto)
+                .Where(o => o.Produto.Ativo && o.Produto.EmpresaID == empresaID).ToList();
+
+            List<EstoqueView> listaProdutos = _context.Produto.Include(o => o.Fornecedor)
+                .Where(o => o.EmpresaID == empresaID && o.Ativo).ToList()
+                .Select(o=>
+                {
+                    int Quantidade = listaEstoque.Where(oo=> oo.ProdutoID == o.ID).Sum(oo=> oo.Quantidade);
+                    return new EstoqueView
+                    {
+                        Quantidade = Quantidade == 0 ? "Não ha estoque" : Quantidade.ToString(),
+                        ProdutoNome = o.Nome,
+                        FornecedorNome = o.Fornecedor.Nome,
+                        ValorKG = o.ValorVendaKG == null ? "Valor não cadastrado" : o.ValorVendaKG.Value.ToString()
+                    };
+                }).ToList();
+            if (listaProdutos.Count == 0) return Ok(new List<EstoqueView>());
+
+            return Ok(listaProdutos);
+        }
+        [HttpGet]
+        public IActionResult Estoque(int produtoID)
+        {
+            var produtos = _context.Produto.Include(o => o.Fornecedor)
+                .SingleOrDefault(o => o.ID == produtoID);
+            if (produtos == null) return BadRequest("Produto não encontrado");
+
+            var listaEstoque = _context.Estoque.Include(o => o.Produto)
+                .Where(o => o.Produto.ID == produtoID).ToList();
+
+            int Quantidade = listaEstoque.Where(o => o.ProdutoID == produtoID).Sum(o => o.Quantidade);
+            var Estoque = new EstoqueView
+            {
+                Quantidade = Quantidade == 0 ? "Não ha estoque" : Quantidade.ToString(),
+                ProdutoNome = produtos.Nome,
+                FornecedorNome = produtos.Fornecedor.Nome,
+                ValorKG = produtos.ValorVendaKG == null ? "Valor não cadastrado" : produtos.ValorVendaKG.Value.ToString()
+            };
+
+            return Ok(Estoque);
+        }
     }
 }
--- Entidades/EstoqueEntidade.cs
+++ Entidades/EstoqueEntidade.cs
@@ -11,5 +11,17 @@
         public ProdutoEntidade Produto { get; set; }
         public int UsuarioID { get; set; }
         public UsuarioEntidade UsuarioADD { get; set; }
-    }            
+    }
+    
+    public class EstoqueView
+    {
+        public EstoqueView()
+        {
+        }
+        public string Quantidade { get; set; }
+        public string ProdutoNome { get; set; }
+        public string FornecedorNome { get; set; }
+        public string ValorKG { get; set; }
+    }
+
 }
